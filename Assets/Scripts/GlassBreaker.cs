92f91762096e99f992d731ac3685b469b371b2a9
Bodaciouslycrazy <bodiemalik@gmail.com>
2015-12-14 22:24:56 -0600

    Lightmap, walls, start of glass.

---
+++ Assets/Scripts/GlassBreaker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlassBreaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	void OnCollisionEnter( Collision col ){
+		//do stuff
+		if(col.gameObject.tag == "Player"){
+			Destroy( gameObject );
+		}
+	}
+}
